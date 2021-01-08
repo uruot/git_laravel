61c00f734872d0b466e0ddbdfcd54d87d11c0474
uruot <zaku2002t@gmail.com>
2021-01-09 00:52:37 +0900

    Users追加

---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\support\Facades\DB;
+use Carbon\Carbon;
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+      DB::table('user')->insert([
+        'name' => 'test taro',
+        'email' => 'test@example.com',
+        'password' => bcrypt('password'),
+        'created_at' => Carbon::now(),
+        'updated_at' => Carbon::now(),
+      ]);
+    }
+}
