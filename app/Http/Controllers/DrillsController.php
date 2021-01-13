c983bc96e1adc1fdc5fe33b7c3b378e9a6faa146
uruot <zaku2002t@gmail.com>
2021-01-13 23:41:49 +0900

    Drills設定付加

--- app/Http/Controllers/DrillsController.php
+++ app/Http/Controllers/DrillsController.php
@@ -11,3 +11,22 @@ class DrillsController extends Controller
       return view('drills.new');
   }
 }
+
+public function create(Request $request)
+  {
+      $request->validate([
+          'title' => 'required|string|max:255',
+          'category_name' => 'required|string|max:255',
+          'problem0' => 'required|string|max:255',
+          'problem1' => 'string|max:255',
+          'problem2' => 'string|max:255',
+          'problem3' => 'string|max:255',
+          'problem4' => 'string|max:255',
+          'problem5' => 'string|max:255',
+          'problem6' => 'string|max:255',
+          'problem7' => 'string|max:255',
+          'problem8' => 'string|max:255',
+          'problem9' => 'string|max:255',
+      ]);
+  }
+}
