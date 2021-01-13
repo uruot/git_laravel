360bdb89086d348f44f1d7e7575c1041be82b3f2
uruot <zaku2002t@gmail.com>
2021-01-14 00:12:05 +0900

    drill.php追加

--- app/Models/Drill.php
+++ app/Models/Drill.php
@@ -7,5 +7,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Drill extends Model
 {
-    use HasFactory;
+  // DBで間違っても変更させたくないカラム（ユーザーID、管理者権限など）にはロックをかけておく事ができる
+// ロックの掛け方にはfillableかguardedの２種類がある。
+// どちらかしか指定できない
+
+// モデルがその属性以外を持たなくなる（fillメソッドに対応しやすいが、カラムが増えるほど足していく必要あり）
+protected $fillable = ['title', 'category_name', 'problem0', 'problem1', 'problem2', 'problem3', 'problem4', 'problem5', 'problem6', 'problem7', 'problem8', 'problem9'];
+// モデルからその属性が取り除かれる（カラムが増えてもほとんど変更しなくて良い）
+protected $guarded = ['id'];
 }
