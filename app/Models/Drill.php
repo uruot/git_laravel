34015e86b4aab6eaaaa8e749db1f4d7a86998394
uruot <zaku2002t@gmail.com>
2021-01-16 23:53:53 +0900

    drill.php設定追加

--- app/Models/Drill.php
+++ app/Models/Drill.php
@@ -15,4 +15,8 @@ class Drill extends Model
 protected $fillable = ['title', 'category_name', 'problem0', 'problem1', 'problem2', 'problem3', 'problem4', 'problem5', 'problem6', 'problem7', 'problem8', 'problem9'];
 // モデルからその属性が取り除かれる（カラムが増えてもほとんど変更しなくて良い）
 protected $guarded = ['id'];
+
+public function user()
+{
+    return $this->belongsTo('App\User');
 }
