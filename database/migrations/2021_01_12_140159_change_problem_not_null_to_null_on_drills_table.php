d5948f9e8b4a8d6e07f7ba11b276c7e44cb12424
uruot <zaku2002t@gmail.com>
2021-01-25 22:22:43 +0900

    設定追加

--- database/migrations/2021_01_12_140159_change_problem_not_null_to_null_on_drills_table.php
+++ database/migrations/2021_01_12_140159_change_problem_not_null_to_null_on_drills_table.php
@@ -14,7 +14,15 @@ class ChangeProblemNotNullToNullOnDrillsTable extends Migration
     public function up()
     {
         Schema::table('drills', function (Blueprint $table) {
-            //
+          $table->text('problem1')->nullable()->change();
+          $table->text('problem2')->nullable()->change();
+          $table->text('problem3')->nullable()->change();
+          $table->text('problem4')->nullable()->change();
+          $table->text('problem5')->nullable()->change();
+          $table->text('problem6')->nullable()->change();
+          $table->text('problem7')->nullable()->change();
+          $table->text('problem8')->nullable()->change();
+          $table->text('problem9')->nullable()->change();
         });
     }
 
@@ -26,7 +34,15 @@ class ChangeProblemNotNullToNullOnDrillsTable extends Migration
     public function down()
     {
         Schema::table('drills', function (Blueprint $table) {
-            //
+          $table->text('problem1')->nullable(false)->change();
+          $table->text('problem2')->nullable(false)->change();
+          $table->text('problem3')->nullable(false)->change();
+          $table->text('problem4')->nullable(false)->change();
+          $table->text('problem5')->nullable(false)->change();
+          $table->text('problem6')->nullable(false)->change();
+          $table->text('problem7')->nullable(false)->change();
+          $table->text('problem8')->nullable(false)->change();
+          $table->text('problem9')->nullable(false)->change();
         });
     }
 }
