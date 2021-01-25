db52a284861aa7d1f6d11ba0b18f804e76194cda
uruot <zaku2002t@gmail.com>
2021-01-25 21:15:56 +0900

    フラッシュメッセージ追加

--- app/Http/Controllers/DrillsController.php
+++ app/Http/Controllers/DrillsController.php
@@ -1,8 +1,8 @@
 <?php
 
 namespace App\Http\Controllers;
-
 use Illuminate\Http\Request;
+use App\Drill;
 
 class DrillsController extends Controller
 {
@@ -16,27 +16,34 @@ class DrillsController extends Controller
       return view('drills.new');
   }
 
-
 public function create(Request $request)
   {
       $request->validate([
           'title' => 'required|string|max:255',
           'category_name' => 'required|string|max:255',
           'problem0' => 'required|string|max:255',
-          'problem1' => 'string|max:255',
-          'problem2' => 'string|max:255',
-          'problem3' => 'string|max:255',
-          'problem4' => 'string|max:255',
-          'problem5' => 'string|max:255',
-          'problem6' => 'string|max:255',
-          'problem7' => 'string|max:255',
-          'problem8' => 'string|max:255',
-          'problem9' => 'string|max:255',
       ]);
-  }
+
+      // モデルを使って、DBに登録する値をセット
+       $drill = new Drill;
+
+       // １つ１つ入れるか
+//        $drill->title = $request->title;
+//        $drill->category_name = $request->category_name;
+//        $drill->save();
+
+       // fillを使って一気にいれるか
+       // $fillableを使っていないと変なデータが入り込んだ場合に勝手にDBが更新されてしまうので注意！
+       $drill->fill($request->all())->save();
+
+       // リダイレクトする
+       // その時にsessionフラッシュにメッセージを入れる
+       return redirect('/drills/new')->with('flash_message', __('Registered.'));
+
+   }
 }
 
-public function destroy($id)
+public function destroy($id);
 {
     // GETパラメータが数字かどうかをチェックする
     if(!ctype_digit($id)){
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -84,6 +84,13 @@
             </div>
         </nav>
 
+        <!-- フラッシュメッセージ -->
+        @if (session('flash_message'))
+            <div class="alert alert-primary text-center" role="alert">
+                {{ session('flash_message') }}
+            </div>
+        @endif
+
         <main class="py-4">
             @yield('content')
         </main>
